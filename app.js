a5694959b8b50d507126f3dc9d5a151eac75bde9
Mesfer <mesfer.vip@gmail.com>
2020-10-20 15:56:44 +0300

    create watch instead of computed

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullName:'',
     };
+  },
+  watch:{
+    name(value){
+      if (value === ''){
+        this.fullName = '';
+      }else{
+    this.fullName = value + ' ' + ' ALQahtani';
+  }
+    },
   },
   computed:{
     fullname(){
-      console.log('Running agin ....');
-      if (this.name === ''){ return '';}
-      return this.name + ' ' + " AlQahtani"; // computed is mush better than mehtod in calculation 
+      // console.log('Running agin ....');
+      // if (this.name === ''){ return '';}
+      // return this.name + ' ' + " AlQahtani"; // computed is mush better than mehtod in calculation 
     }
   },
   methods: {
-    fullName(){
-     
-    },
-
+   
     setName(event, lastName) {
       this.name = event.target.value;
     },
