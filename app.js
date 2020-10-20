fb41325bc25770125f58b7924314ec58c7e63e5d
Mesfer <mesfer.vip@gmail.com>
2020-10-20 16:44:22 +0300

    add another way or approach

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const app = Vue.createApp({
     };
   },
   watch:{
+
     counter(value){
-      if (value > 50){
-         this.counter = 0 ;
+      if(value > 50 || value < -50){
+        const that = this;
+        setTimeout(function(){
+          that.counter = 0;
+        })
       }
-    }
+    },
+    // counter(value){
+    //   if (value > 50){
+    //      this.counter = 0 ;
+    //   }
+    // }
   //   name(value){
   //     if (value === ''){
   //       this.fName = '';
